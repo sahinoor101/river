dac7099600c863a9ba0304dd56ac984451becaf0
sahinoor101 <sahinoor101@gmail.com>
2024-11-19 22:55:36 +0530

    error solved

--- pathFinder.java
+++ pathFinder.java
@@ -1,21 +1,20 @@
-
 class pathFinder {
 
     public static void main(String args[]) {
         pathFinder ob = new pathFinder();
-        int mat[][] = {{0, 0, 0, 1, 0}, {0, 1, 1, 1, 0}, {0, 1, 0, 0, 0}, {0, 1, 1, 0, 0}, {0, 0, 1, 1, 0}};
+        int mat[][] = {{0, 0, 0, 1, 0}, {0, 1, 1, 1, 0}, {0, 1, 0, 0, 0}, {0, 1, 1, 0, 0}, {0, 0, 1, 1, 1}};
         int rows, cols, start, i, j;
         rows = mat.length;
         cols = mat[0].length;
         start = ob.start(mat);
-        System.out.println((cols - 1) + "," + start);
         for (i = 0; i < rows; i++) {
             for (j = 0; j < cols; j++) {
                 System.out.print(" " + mat[i][j]);
             }
             System.out.println();
         }
-        ob.path((cols - 1), start, mat, 1);
+        System.out.println((cols - 1) + "," + start);
+        ob.path((cols - 1), start, mat, 1,start);
     }
 
     public int start(int mat[][]) {
@@ -24,6 +23,17 @@ class pathFinder {
         cols = mat[0].length;
         for (int i = (cols - 1); i > 0; i--) {
             if (mat[(rows - 1)][i] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public int end(int mat[][])
+    {
+        int rows;
+        rows = mat.length;
+        for (int i = 0; i<rows; i++) {
+            if (mat[0][i] == 1) {
                 return i;
             }
         }
@@ -40,7 +50,7 @@ class pathFinder {
         return true;
     }
 
-    public void path(int x, int y, int[][] mat, int count) {
+    public void path(int x, int y, int[][] mat, int count,int st) {
         pathFinder ob = new pathFinder();
         int rows, cols, i, j;
         rows = mat.length;
@@ -50,7 +60,7 @@ class pathFinder {
         }
 
         if (count == 2) {
-            mat[rows-1][ob.start(mat)] = 1;
+            mat[rows-1][st] = 1;
         }
         if (ob.isValid(x + 1, y, mat) == true && mat[x + 1][y] == 1) {
             mat[x][y] = count;
@@ -58,7 +68,7 @@ class pathFinder {
                 mat[x][y] = 0;
             }
             System.out.println((x + 1) + "," + y);
-            path(x + 1, y, mat, count + 1);
+            path(x + 1, y, mat, count + 1,st);
         }
         if (ob.isValid(x - 1, y, mat) == true && mat[x - 1][y] == 1) {
             mat[x][y] = count;
@@ -66,7 +76,7 @@ class pathFinder {
                 mat[x][y] = 0;
             }
             System.out.println((x - 1) + "," + y);
-            path(x - 1, y, mat, count + 1);
+            path(x - 1, y, mat, count + 1,st);
         }
         if (ob.isValid(x, y + 1, mat) == true && mat[x][y + 1] == 1) {
             mat[x][y] = count;
@@ -74,7 +84,7 @@ class pathFinder {
                 mat[x][y] = 0;
             }
             System.out.println(x + "," + (y + 1));
-            path(x, y + 1, mat, count + 1);
+            path(x, y + 1, mat, count + 1,st);
         }
         if (ob.isValid(x, y - 1, mat) == true && mat[x][y - 1] == 1) {
             mat[x][y] = count;
@@ -82,9 +92,9 @@ class pathFinder {
                 mat[x][y] = 0;
             }
             System.out.println(x + "," + (y - 1));
-            path(x, y - 1, mat, count + 1);
+            path(x, y - 1, mat, count + 1,st);
         }
-        if (x == 0) 
+        if (x==0 && y==ob.end(mat)) 
         {
             mat[0][y]=count;
             for (i = 0; i < rows; i++) {
