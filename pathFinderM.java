71864a79474b90a148f2419394748975d3c133fe
sahinoor101 <sahinoor101@gmail.com>
2024-11-20 11:45:30 +0530

    Multiple rivers

---
+++ pathFinderM.java
@@ -0,0 +1,105 @@
+class pathFinderM {
+
+    public static void main(String args[]) {
+        pathFinderM ob = new pathFinderM();
+        int mat[][] = {{0, 0, 0, 1, 0}, {0, 1, 1, 1, 0}, {0, 1, 0, 0, 0}, {0, 1, 1, 0, 0}, {0, 0, 1, 1, 1}};
+        int rows, cols, start, i, j;
+        rows = mat.length;
+        cols = mat[0].length;
+        start = ob.start(mat);
+        for (i = 0; i < rows; i++) {
+            for (j = 0; j < cols; j++) {
+                System.out.print(" " + mat[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println((cols - 1) + "," + start);
+        ob.path((cols - 1), start, mat, 1,start);
+    }
+
+    public int start(int mat[][]) {
+        int rows, cols;
+        rows = mat.length;
+        cols = mat[0].length;
+        for (int i = (cols - 1); i > 0; i--) {
+            if (mat[(rows - 1)][i] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public int end(int mat[][])
+    {
+        int rows;
+        rows = mat.length;
+        for (int i = 0; i<rows; i++) {
+            if (mat[0][i] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean isValid(int x, int y, int mat[][]) {
+        int rows, cols;
+        rows = mat.length;
+        cols = mat[0].length;
+        return !(x < 0 || y < 0 || x >= rows || y >= cols);
+    }
+
+    public void path(int x, int y, int[][] mat, int count,int st) {
+        pathFinderM ob = new pathFinderM();
+        int rows, cols, i, j;
+        rows = mat.length;
+        cols = mat[0].length;
+        if (ob.isValid(x, y, mat) == false) {
+            return;
+        }
+
+        if (count == 2) {
+            mat[rows-1][st] = 1;
+        }
+        if (ob.isValid(x + 1, y, mat) == true && mat[x + 1][y] == 1) {
+            mat[x][y] = count;
+            if (count == 1) {
+                mat[x][y] = 0;
+            }
+            System.out.println((x + 1) + "," + y);
+            path(x + 1, y, mat, count + 1,st);
+        }
+        if (ob.isValid(x - 1, y, mat) == true && mat[x - 1][y] == 1) {
+            mat[x][y] = count;
+            if (count == 1) {
+                mat[x][y] = 0;
+            }
+            System.out.println((x - 1) + "," + y);
+            path(x - 1, y, mat, count + 1,st);
+        }
+        if (ob.isValid(x, y + 1, mat) == true && mat[x][y + 1] == 1) {
+            mat[x][y] = count;
+            if (count == 1) {
+                mat[x][y] = 0;
+            }
+            System.out.println(x + "," + (y + 1));
+            path(x, y + 1, mat, count + 1,st);
+        }
+        if (ob.isValid(x, y - 1, mat) == true && mat[x][y - 1] == 1) {
+            mat[x][y] = count;
+            if (count == 1) {
+                mat[x][y] = 0;
+            }
+            System.out.println(x + "," + (y - 1));
+            path(x, y - 1, mat, count + 1,st);
+        }
+        if (x==0 && y==ob.end(mat)) 
+        {
+            mat[0][y]=count;
+            for (i = 0; i < rows; i++) {
+                for (j = 0; j < cols; j++) {
+                    System.out.print(" " + mat[i][j]);
+                }
+                System.out.println();
+            }
+        }
+    }
+}
